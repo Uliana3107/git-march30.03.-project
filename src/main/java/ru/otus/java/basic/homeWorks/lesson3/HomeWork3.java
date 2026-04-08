cd9fe950b334018cbb3cdffe48308985b9be4379
Uliana <alexul17@mail.ru>
2026-04-08 14:28:41 +0300

    Commit HomeWork3 for lesson3.1

--- src/main/java/ru/otus/java/basic/homeWorks/lesson3/HomeWork3.java
+++ src/main/java/ru/otus/java/basic/homeWorks/lesson3/HomeWork3.java
@@ -1,21 +1,29 @@
 package ru.otus.java.basic.homeWorks.lesson3;
 
 public class HomeWork3 {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         calc();
         text();
     }
-     public static void calc () {
+
+    public static void calc() {
         int a = 6;
         int b = 50;
         int c = 100;
         int result;
-        if (a>b) {
+        if (a > b) {
             result = b + c;
         } else result = a + c;
         System.out.println(result);
-     }
-     public static void text () {
-        System.out.println("Hello, World");
-     }
-}
+    }
+
+    public static void text() {
+        int r = 15;
+        r++;
+        r = r / 2;
+        if (r > 0) {
+            System.out.println("Hello, World");
+        }
+
+
+    }}
